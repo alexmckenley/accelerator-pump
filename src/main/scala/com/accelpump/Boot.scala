d48003d8c4f0395575d4017125d9357448636fbd
Alex Taylor <alexmckenley@gmail.com>
2015-09-02 13:39:51 -0700

    Restructure boot.scala

--- src/main/scala/com/accelpump/Boot.scala
+++ src/main/scala/com/accelpump/Boot.scala
@@ -5,21 +5,21 @@ import akka.io.IO
 import spray.can.Http
 import akka.pattern.ask
 import akka.util.Timeout
+import scala.concurrent._
 import scala.concurrent.duration._
 import scalikejdbc._
 import scalikejdbc.config._
 
 object Boot extends App {
 
-  implicit val system = ActorSystem("accelerator-pump")
+  AcceleratorPump.initialize()
+  AcceleratorPump.system.log.info("Booting AcceleratorPump...")
 
-  // create and start the ApiServiceActor
-  val apiService = system.actorOf(Props[ApiServiceActor], "api-service")
-
-  implicit val timeout = Timeout(5.seconds)
-
-  // Move this to a better place
-  DBs.setupAll()
+  AcceleratorPump.startHttpServer().onFailure {
+    case ex:Throwable =>
+      AcceleratorPump.system.log.error(ex, "Error starting HTTP server, exiting...")
+      AcceleratorPump.system.shutdown()
+  }(AcceleratorPump.system.dispatcher)
 
   val projects = DB readOnly { implicit session =>
     sql"select * from projects".map(rs => (rs.string("name"), rs.long("id"))).list.apply()
@@ -27,6 +27,27 @@ object Boot extends App {
 
   println(projects)
 
-  // start the HTTP server
-  IO(Http) ? Http.Bind(apiService, interface = "localhost", port = 8080)
+}
+
+object AcceleratorPump {
+  implicit val system = ActorSystem("accelerator-pump")
+  implicit val timeout = Timeout(5.seconds)
+
+  def initialize():Unit = {
+    startDbConnection()
+  }
+
+  def startDbConnection(): Unit = {
+    DBs.setupAll()
+    system.registerOnTermination {
+      system.log.info("Shutting down MySQL connection pool...")
+      DBs.closeAll()
+    }
+  }
+
+  def startHttpServer(): Future[Unit] = {
+    import system.dispatcher
+    val apiService = AcceleratorPump.system.actorOf(Props[ApiServiceActor], "api-service")
+    (IO(Http) ? Http.Bind(apiService, interface = "localhost", port = 8080)).map(_ => Unit)
+  }
 }
